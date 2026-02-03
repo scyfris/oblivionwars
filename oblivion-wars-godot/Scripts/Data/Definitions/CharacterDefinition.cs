a3bc6ca147e2c8d3bad6ce53db5645a58d7fc988
scyfris <scyfris@gmail.com>
2026-02-03 14:06:37 -0600

    Checkpoint before additional changes to fix some things.

--- oblivion-wars-godot/Scripts/Data/Definitions/CharacterDefinition.cs
+++ oblivion-wars-godot/Scripts/Data/Definitions/CharacterDefinition.cs
@@ -22,13 +22,13 @@ public partial class CharacterDefinition : Resource
     [ExportGroup("Movement")]
     [Export] public float JumpStrength = 800.0f;
     [Export] public float WallJumpStrength = 700.0f;
+    [Export] public float WallJumpPushAwayForce = 500.0f;
+    [Export] public float WallJumpPushAwayDuration = 0.2f;
+    [Export] public float WallJumpInputLockDuration = 0.2f;
 
     [ExportGroup("Physics")]
     [Export] public float Gravity = 2000.0f;
     [Export] public float WallSlideSpeedFraction = 0.5f;
-    [Export] public float WallJumpPushAwayForce = 500.0f;
-    [Export] public float WallJumpPushAwayDuration = 0.2f;
-    [Export] public float WallJumpInputLockDuration = 0.2f;
 
     [ExportGroup("Persistence")]
     [Export] public PersistenceMode Persistence = PersistenceMode.None;
